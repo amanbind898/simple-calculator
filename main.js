604e2c5210aece002bc38e2e9ebe2557d665144b
amanbind898 <aman.2201086cs@iiitbh.ac.in>
2023-09-07 16:38:56 +0530

    files added

---
+++ main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let string = '';
+    let buttons = document.querySelectorAll('.button');
+    let inputField = document.querySelector('.input');
+    let keypressSound = document.getElementById('keypressSound');
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            let buttonText = e.target.innerHTML;
+if(buttonText==='X')
+buttonText='*'
+            if (buttonText === '=') {
+                try {
+                    string = eval(string);
+                    inputField.value = string;
+                } catch (error) {
+                    inputField.value = 'Error';
+                }
+            }
+            else if(buttonText === 'DEL') 
+            {
+                string=string.substring(0,string.length-1);
+                inputField.value=string;
+            }
+            else if (buttonText === 'AC') {
+                string = '0'; // Set the input to '0' on All Clear
+                inputField.value = string;
+            } else {
+                if (string === '0') {
+                    string = buttonText; // Replace '0' with the new button value
+                } 
+               
+                else {
+                    string = string + buttonText;
+                }
+                inputField.value = string;
+            }
+             // Play the keypress sound
+             keypressSound.play();
+        });
+    });
+});

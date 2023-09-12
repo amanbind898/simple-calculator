cb75c3babbb0328097fa448549dfa53bb94bd192
amanbind898 <aman.2201086cs@iiitbh.ac.in>
2023-09-12 20:18:36 +0530

    audio updated

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.querySelectorAll('.button');
     let inputField = document.querySelector('.input');
     let keypressSound = document.getElementById('keypressSound');
+
+    // Preload the audio file
+    keypressSound.preload = 'auto';
+
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
             let buttonText = e.target.innerHTML;
-if(buttonText==='X')
-buttonText='*'
+
+            // Play the keypress sound immediately
+            keypressSound.currentTime = 0;
+            keypressSound.play();
+
+            if (buttonText === 'X') {
+                buttonText = '*';
+            }
+            
             if (buttonText === '=') {
                 try {
                     string = eval(string);
@@ -15,27 +26,20 @@ buttonText='*'
                 } catch (error) {
                     inputField.value = 'Error';
                 }
-            }
-            else if(buttonText === 'DEL') 
-            {
-                string=string.substring(0,string.length-1);
-                inputField.value=string;
-            }
-            else if (buttonText === 'AC') {
+            } else if (buttonText === 'DEL') {
+                string = string.substring(0, string.length - 1);
+                inputField.value = string;
+            } else if (buttonText === 'AC') {
                 string = '0'; // Set the input to '0' on All Clear
                 inputField.value = string;
             } else {
                 if (string === '0') {
                     string = buttonText; // Replace '0' with the new button value
-                } 
-               
-                else {
+                } else {
                     string = string + buttonText;
                 }
                 inputField.value = string;
             }
-             // Play the keypress sound
-             keypressSound.play();
         });
     });
 });
